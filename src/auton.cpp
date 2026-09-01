877e035d7e24fc0c30b7225ba8d20d63874da1f5
an <nahata.ansh@gmail.com>
2026-08-31 20:46:16 -0700

    Update auton.cpp

--- src/auton.cpp
+++ src/auton.cpp
@@ -7,9 +7,16 @@
 #include "pros/motors.h"
 #include "pros/rtos.h"
 #include "pros/rtos.hpp"
-#include "main.h"
 #include <cstdio>
 #include "auton.h"
+
+extern lemlib::Chassis chassis;
+extern pros::Motor lift;
+extern pros::Motor claw;
+extern pros::Distance backdistance; 
+extern pros::Distance frontdistance;
+extern pros::Distance leftdistance;
+extern pros::Distance rightdistance;
 
 float getBack() {
     return (backdistance.get()/25.4) + 0; // change these values to adjust for the distance from the sensor to the center of the robot
@@ -28,41 +35,41 @@ float getRight() {
 }
 
 //preliminary auton using no sensors
-void noSensorAuton() {
-    //robot switches toggle before compmleting rest of routine
-    chassis.move(-10, inches);
-    pros::c::delay(250);
-    chassis.move(10, inches);
-    pros::c::delay(250);
-    //robot moves towards alliance goal
-    chassis.move(-24, inches);
-    chassis.turn(90, degrees);
-    lift.move_absolute(300, degrees);
-    chassis.move(-20, inches);
-    //robot scores on alliance goal
-    lift.move_absolute(200, degrees);
-    claw.move(120);
-    pros::c::delay(100);
-    claw.stop();
-    //robot backs away from alliance goal and moves towards nearby stack
-    chassis.move(20, inches);
-    chassis.turn(-45, degrees);
-    chassis.setDriveVelocity(50, percent);
-    chassis.drive(-23, inches);
-    //robot picks up nearby stack and moves towards alliance goal
-    claw.move(-120);
-    pros::c::delay(100);
-    claw.stop
-    chassis.setDriveVelocity(100, percent);
-    chassis.turn(-135, degrees);
-    lift.move_absolute(300, degrees);
-    chassis.move(20, inches);
-    //robot scores on alliance goal
-    lift.move_absolute(200, degrees);
-    claw.move(120);
-    pros::c::delay(100);
-    claw.stop();
-}
+// void noSensorAuton() {
+//     //robot switches toggle before compmleting rest of routine
+//     chassis.move(-10);
+//     pros::c::delay(250);
+//     chassis.move(10);
+//     pros::c::delay(250);
+//     //robot moves towards alliance goal
+//     chassis.move(-24);
+//     chassis.turn(90);
+//     lift.move_absolute(300);
+//     chassis.move(-20);
+//     //robot scores on alliance goal
+//     lift.move_absolute(200);
+//     claw.move(120);
+//     pros::c::delay(100);
+//     claw.stop();
+//     //robot backs away from alliance goal and moves towards nearby stack
+//     chassis.move(20);
+//     chassis.turn(-45);
+//     chassis.setDriveVelocity(50);
+//     chassis.drive(-23);
+//     //robot picks up nearby stack and moves towards alliance goal
+//     claw.move(-120);
+//     pros::c::delay(100);
+//     claw.stop
+//     chassis.setDriveVelocity(100);
+//     chassis.turn(-135);
+//     lift.move_absolute(300);
+//     chassis.move(20);
+//     //robot scores on alliance goal
+//     lift.move_absolute(200);
+//     claw.move(120);
+//     pros::c::delay(100);
+//     claw.stop();
+// }
 
 //main 15 second auton
 
@@ -71,12 +78,12 @@ void mainAuton() {
     //move away from toggle
     chassis.moveToPoint(-48, 0, 2000);
     //move towards alliance goal
-    chassis.turnToHeading(-90, degrees, 2000);
-    lift.move_absolute(300, degrees);
+    chassis.turnToHeading(-90, 2000);
+    lift.move_absolute(300);
     chassis.setPose(chassis.getPose().x, chassis.getPose().y, getLeft(), chassis.getPose().theta); //insert values as x, y, sensor reading, theta
     chassis.moveToPoint(-48, 22, 1000, {.forwards = false});
     //score in alliance goal
-    lift.move_absolute(200, degrees);
+    lift.move_absolute(200);
     claw.move(120);
     pros::c::delay(100);
     claw.stop
@@ -91,9 +98,9 @@ void mainAuton() {
     chassis.moveToPoint(-48, 12, 1000);
     chassis.turnToPoint(-48, 24, 1000);
     chassis.moveToPoint(-48, 22, 1000);
-    lift.move_absolute(300, degrees);
+    lift.move_absolute(300);
     //score on alliance goal
-    lift.move_absolute(200, degrees);
+    lift.move_absolute(200);
     claw.move(120);
     pros::c::delay(100);
     claw.stop();
